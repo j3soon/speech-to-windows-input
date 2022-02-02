a81b7ff307d59f78fd91425f4c90bdb0ba0c3073
Johnson <j3.soon@msa.hinet.net>
2022-02-02 20:46:07 +0800

    Proof of Concept

---
+++ README.md
@@ -0,0 +1,77 @@
+# Speech to Windows Input
+
+## How to Use
+
+Prerequisites:
+- .Net Framework >= 4.6.1
+- An Azure subscription key for the Speech service. See [Try the speech service for free](https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/overview#try-the-speech-service-for-free).
+- A Windows PC with a working microphone.
+
+Copy the Azure key and region text into the config file `config.json`.
+
+## Default Config
+
+```json
+{
+  "AzureSubscriptionKey": "<paste-your-subscription-key>",
+  "AzureServiceRegion": "<paste-your-region>",
+  "Languages": [
+    "en-US",
+    "zh-TW"
+  ],
+  "PhraseList": [],
+  "PrioritizeLatencyOrAccuracy": "Latency",
+  "SoundEffect": false
+}
+```
+
+Make sure to replace `<paste-your-subscription-key>` and `<paste-your-region>` to valid Azure credentials, otherwise, an error will be raised.
+
+- `AzureSubscriptionKey`: Your Azure subscription key.
+- `AzureServiceRegion`: Your Azure subscription region. (e.g., `"westus"` or `"eastasia"`)
+- `Languages`: A list of languages to perform speech recognition. (No mixed language recognition) See the [supported languages](https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/language-support#speech-to-text) for more information.
+- `PhraseList`: A list of of custom phrases such as names, technical terms, etc. For an example, if you are a gamer that types `GG` (i.e., Good Game) a lot, you will want to add `GG` in this list. Otherwise, Azure will recognize it as `JuJu`, `Gigi`, etc.
+- `PrioritizeLatencyOrAccuracy`: Select the recognition mode between `"Latency"` and `"Accuracy"`.
+- `SoundEffect`: Determines whether the program should play a hint sound when the speech recognition starts/stops.
+
+## Program Hint
+
+```
+speech-to-windows-input (made by j3soon)
+1. Press Win+H to convert speech to text input. The recognition stops on microphone silence or after 15 seconds.
+2. The on-going speech recognition cannot be cancelled. (please wait until 15 seconds is reached)
+3. Press Ctrl+C to exit.
+Notes:
+- Requires internet.
+- The default microphone is used for speech recognition.
+- If input fails for certain applications, you may need to launch this program with `Run as administrator`.
+```
+
+## Side Notes
+
+Runtime:
+- If Unicode characters (e.g., Chinese) cannot be shown correctly, you should set the console font to support Unicode.
+- If you don't want your credit card be accidentally charged, set the Pricing Tier to `Free F0` instead of `Standard F0` (i.e., Pay as You Go). The free tier has [5 hours audio quota each month](https://azure.microsoft.com/en-us/pricing/details/cognitive-services/speech-services/).
+
+Compilation:
+- `SpeechConfig.FromSubscription` will report `BadImageFormatException` if `AnyCPU` is used instead of `x86`/`x64`.
+- Must compile with `x86` config since LowLevelControls [does not work in x64 config](https://github.com/j3soon/LowLevelControls/issues/1).
+
+## References
+
+Minimal Sample:
+- [Quickstart: Recognize speech from a microphone in C# under .NET Framework for Windows](https://github.com/Azure-Samples/cognitive-services-speech-sdk/tree/master/quickstart/csharp/dotnet/from-microphone)
+- [Get started with speech-to-text](https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/get-started-speech-to-text?pivots=programming-language-csharp&tabs=windowsinstall)
+
+Additional Features:
+(✔️ means implemented, ❌ means not)
+- ✔️ [Automatic language detection](https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/how-to-automatic-language-detection?pivots=programming-language-csharp)
+  Pre-define a list of languages. Azure Speech SDK will identify the languages used before starting speech recognition. (i.e., Language Detection -> Speech Recognition for that specific language). Speech recognition for mixed languages is not supported at the time of writing.
+- ✔️ [Recognizing custom phrases](https://docs.microsoft.com/en-us/dotnet/api/microsoft.cognitiveservices.speech.phraselistgrammar?view=azure-dotnet)
+  Allows recognition of custom phrases such as names, technical terms, etc.
+- ❌ [Detailed results](https://docs.microsoft.com/en-us/dotnet/api/microsoft.cognitiveservices.speech.detailedspeechrecognitionresult?view=azure-dotnet)
+  Reports the recognition confidence, and the actual words recognized.
+- ❌ [Continuous recognition](https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/get-started-speech-to-text?tabs=windowsinstall&pivots=programming-language-csharp#continuous-recognition)
+  Instead of performing speech recognition until silence or reaching 15 seconds, let user choose when to stop the recognition.
+- ❌ [Select an audio input device](https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/how-to-select-audio-input-devices)
+  Instead of the default microphone, let the user choose the preferred microphone.
---
+++ speech-to-windows-input/Program.cs
@@ -0,0 +1,194 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System;
+using System.IO;
+using System.Media;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using llc = LowLevelControls;
+
+namespace speech_to_windows_input
+{
+    public class Config
+    {
+        public String AzureSubscriptionKey { get; set; } = "<paste-your-subscription-key>";
+        public String AzureServiceRegion { get; set; } = "<paste-your-region>"; // (e.g., "westus" or "eastasia")
+        // All languages ref: https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/language-support#speech-to-text
+        public String[] Languages { get; set; } = { "en-US", "zh-TW" };
+        public String[] PhraseList { get; set; } = { };
+        public String PrioritizeLatencyOrAccuracy { get; set; } = "Latency";
+        public bool SoundEffect { get; set; } = false;
+    }
+    public class SpeechRecognitionResult
+    {
+        public String Text = null;
+        public String ErrorMessage = null;
+    }
+    class Program
+    {
+        static Config config = new Config();
+        static llc.KeyboardHook kbdHook = new llc.KeyboardHook();
+        static SpeechRecognizer speechRecognizer;
+        static bool loop = true; // mutex isn't necessary since both Main and Application.DoEvents (WinProc) is in the main thread.
+        static bool keyHDown = false;
+        static Task<SpeechRecognitionResult> task = null;
+        // static void 
+        static void Main(string[] args)
+        {
+            // Tutorial
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("speech-to-windows-input (made by j3soon)");
+            Console.WriteLine("1. Press Win+H to convert speech to text input. The recognition stops on microphone silence or after 15 seconds.");
+            // Console.WriteLine("2. Press ESC to cancel the on-going speech recognition (no input will be generated).");
+            Console.WriteLine("2. The on-going speech recognition cannot be cancelled. (please wait until 15 seconds is reached)");
+            Console.WriteLine("3. Press Ctrl+C to exit.");
+            Console.WriteLine("Notes:");
+            Console.WriteLine("- Requires internet.");
+            Console.WriteLine("- The default microphone is used for speech recognition.");
+            Console.WriteLine("- If input fails for certain applications, you may need to launch this program with `Run as administrator`.");
+            // Generate and Load Config
+            var jsonConfig = JsonSerializer.Serialize(config, new JsonSerializerOptions {
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                WriteIndented = true,
+            });
+            var configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.json");
+            if (!File.Exists(configPath))
+                File.WriteAllText(configPath, jsonConfig);
+            jsonConfig = File.ReadAllText(configPath);
+            Console.WriteLine("Your current configuration: " + jsonConfig);
+            try
+            {
+                config = JsonSerializer.Deserialize<Config>(jsonConfig);
+            }
+            catch (JsonException e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.WriteLine("\nError occurred when parsing JSON config, press any key to exit.");
+                Console.ReadKey();
+                return;
+            }
+            // Install Keyboard Hook and Callbacks
+            kbdHook.KeyDownEvent += kbdHook_KeyDownEvent;
+            kbdHook.KeyUpEvent += kbdHook_KeyUpEvent;
+            kbdHook.InstallGlobalHook();
+            Console.CancelKeyPress += Console_CancelKeyPress;
+            // Init Speech Recognizer
+            InitSpeechRecognizer();
+            // Message Loop with Windows.Forms for simplicity (instead of custom WindowProc callback)
+            while (loop)
+            {
+                // Deal with Low Level Hooks Callback
+                Application.DoEvents();
+                // Deal with Speech Recognition Result
+                if (task != null && task.IsCompleted)
+                {
+                    if (task.Result.ErrorMessage == null)
+                    {
+                        Console.WriteLine($"Recognized Text: {task.Result.Text}");
+                        llc.Keyboard.SendText(task.Result.Text);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: {task.Result.ErrorMessage}");
+                    }
+                    task = null;
+                    Console.WriteLine($"[{DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")}] Speech Recognition Done");
+                    if (config.SoundEffect)
+                        SystemSounds.Exclamation.Play();
+                }
+                Thread.Sleep(1);
+            }
+            // Uninstall Ketboard Hook
+            kbdHook.UninstallGlobalHook();
+        }
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = true;
+            loop = false;
+        }
+        private static bool kbdHook_KeyDownEvent(llc.KeyboardHook sender, uint vkCode, bool injected)
+        {
+            if (vkCode == (uint)Keys.H && !keyHDown)
+            {
+                keyHDown = true;
+                if (llc.Keyboard.IsKeyDown((int)Keys.LWin) || llc.Keyboard.IsKeyDown((int)Keys.RWin)) {
+                    if (task != null)
+                        return false;
+                    if (task == null)
+                    {
+                        Console.WriteLine($"[{DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")}] Speech Recognition Started");
+                        if (config.SoundEffect)
+                            SystemSounds.Exclamation.Play();
+                        task = SpeechToTextAsync();
+                    }
+                    return true;
+                }
+            }
+            /*else if (vkCode == (uint)Keys.Escape)
+            {
+                if (task != null)
+                {
+                    cancelled = true;
+                    Console.WriteLine($"[{DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")}] Speech Recognition Cancelled");
+                    return true;
+                }
+            }*/
+            return false;
+        }
+        private static bool kbdHook_KeyUpEvent(llc.KeyboardHook sender, uint vkCode, bool injected)
+        {
+            if (vkCode == (uint)Keys.H)
+                keyHDown = false;
+            return false;
+        }
+        private static void InitSpeechRecognizer()
+        {
+            // Creates an instance of a speech config with specified subscription key and service region.
+            var speechConfig = SpeechConfig.FromSubscription(config.AzureSubscriptionKey, config.AzureServiceRegion);
+            speechConfig.SetProperty(PropertyId.SpeechServiceConnection_SingleLanguageIdPriority, config.PrioritizeLatencyOrAccuracy);
+            var autoDetectSourceLanguageConfig = AutoDetectSourceLanguageConfig.FromLanguages(config.Languages);
+            var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            speechRecognizer = new SpeechRecognizer(speechConfig, autoDetectSourceLanguageConfig, audioConfig);
+            PhraseListGrammar phraseListGrammar = PhraseListGrammar.FromRecognizer(speechRecognizer);
+            foreach (var phrase in config.PhraseList)
+                phraseListGrammar.AddPhrase(phrase);
+        }
+        private static async Task<SpeechRecognitionResult> SpeechToTextAsync()
+        {
+            // Starts speech recognition, and returns after a single utterance is recognized. The end of a
+            // single utterance is determined by listening for silence at the end or until a maximum of 15
+            // seconds of audio is processed.  The task returns the recognition text as result. 
+            // Note: Since RecognizeOnceAsync() returns only a single utterance, it is suitable only for single
+            // shot recognition like command or query. 
+            // For long-running multi-utterance recognition, use StartContinuousRecognitionAsync() instead.
+            var result = await speechRecognizer.RecognizeOnceAsync();
+            var ret = new SpeechRecognitionResult();
+
+            // Checks result.
+            if (result.Reason == ResultReason.RecognizedSpeech)
+            {
+                ret.Text = result.Text;
+            }
+            else if (result.Reason == ResultReason.NoMatch)
+            {
+                ret.ErrorMessage = $"NOMATCH: Speech could not be recognized.";
+            }
+            else if (result.Reason == ResultReason.Canceled)
+            {
+                var cancellation = CancellationDetails.FromResult(result);
+                ret.ErrorMessage = $"CANCELED: Reason={cancellation.Reason}";
+
+                if (cancellation.Reason == CancellationReason.Error)
+                {
+                    ret.ErrorMessage += $"\nCANCELED: ErrorCode={cancellation.ErrorCode}";
+                    ret.ErrorMessage += $"\nCANCELED: ErrorDetails={cancellation.ErrorDetails}";
+                    ret.ErrorMessage += $"\nCANCELED: Did you update the subscription info?";
+                }
+            }
+            return ret;
+        }
+    }
+}
+
